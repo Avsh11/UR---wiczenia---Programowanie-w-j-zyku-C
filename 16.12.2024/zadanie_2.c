bcaa3a00f235051efa757639b76515c6653cd710
Krystian <96619815+Avsh11@users.noreply.github.com>
2024-12-14 22:36:14 +0100

    Add files via upload

---
+++ 16.12.2024/zadanie_2.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h> 
+
+//Zadanie 2 - Napisz program, w którym zadeklarujesz tablicę a następnie:
+// • Wypisz na ekran adres jej pierwszego elementu.
+// • Wypisz na ekran adres jej czwartego elementu.
+// • Napisz program, w którym wylosujesz wartości do tablicy z przedziału podanego przez
+// użytkownika i wypiszesz ich adresy.
+
+
+
+int main()
+{
+	int tab[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int minimum;
+	int maximum;
+
+	printf("Adres pierwszego elemmentu tablicy wynosi: %p \n", &tab[0]);
+	printf("Adres czwartego elementu tablicy wynosi: %p \n", &tab[3]);
+
+	printf("Podaj minimalny zakres przedzialu: ");
+	scanf_s("%d", &minimum);
+	printf("Podaj maksymalny zakres przedzialu: ");
+	scanf_s("%d", &maximum);
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		tab[i] = minimum + rand() & (maximum - minimum + 1);
+		printf("tab[%d] = %d, Adresy wylosowanych wartosci: %p \n",i, tab[i], &tab[i]);
+	}
+
+	return 0;
+}
+
---
+++ 16.12.2024/zadanie_3.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+//Zadanie 3 - Napisz program, w którym wczytasz elementy tablicy, obliczysz ich średnią oraz wpiszesz
+// elementy większe od średniej.
+
+int main()
+{
+	int n;
+
+	printf("Podaj ilosc elementow twojej tablicy: \n");
+	scanf_s("%d", &n);
+
+	if (n <= 0)
+	{
+		printf("Tablica musi miec jakies elementy! \n");
+		return 1;
+	}
+
+	int* tab = (int*)malloc(n * sizeof(int));
+	if (!tab)
+	{
+		printf("Blad alokacji pamieci! \n");
+		return 1;
+	}
+
+	printf("Wprowadz %d elementy tablicy: \n", n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &tab[i]);
+	}
+
+	int suma = 0;
+	for (int i = 0; i < n; i++)
+	{
+		suma += tab[i];
+	}
+	float srednia = suma / (float)n;
+
+	printf("Srednia elementow tablicy wynosi: \n", srednia);
+
+	printf("Liczby wieksze od sredniej to: \n");
+	for (int i = 0; i < n; i++)
+	{
+		if (tab[i] > srednia) {
+			printf("%d", tab[i]);
+		}
+	}
+
+
+	free(tab);
+	return 0;
+}
