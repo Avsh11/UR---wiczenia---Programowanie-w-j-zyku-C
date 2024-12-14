fc405e1495e2405f313412c98731af224c859816
Krystian <96619815+Avsh11@users.noreply.github.com>
2024-12-14 23:34:04 +0100

    Add files via upload

---
+++ 16.12.2024/zadanie_7.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+//Zadanie 7 - Napisz funkcję otrzymującą jako argumenty wskaźniki do dwóch zmiennych typu int, która:
+// • zwraca jako wartość mniejszą z liczb wskazywanych przez argumenty.
+// • zwraca jako wartość wskaźnik na zmienną przechowującą mniejszą z liczb wskazywanych
+// przez argumenty.
+
+int* liczby_m(int* a, int* b)
+{
+	static int m;
+	if (*a < *b)
+	{
+		m = *a;
+	}
+	else {
+		m = *b;
+	}
+	return &m;
+}
+
+int main()
+{
+	int x;
+	int y;
+	printf("Podaj pierwsza liczbe: \n");
+	scanf_s("%d", &x);
+	printf("Podaj druga liczbe: \n");
+	scanf_s("%d", &y);
+
+	int* mniejsza = liczby_m(&x, &y);
+
+
+	printf("Mniejsza liczba jest: %d \n", *mniejsza);
+	printf("Adres tej mniejszej liczby wynosi: %p \n", (void*)mniejsza);
+
+	return 0;
+}
