d9aedc4df6299da6142d3e43d262824e854c0224
Krystian <96619815+Avsh11@users.noreply.github.com>
2024-12-14 23:20:36 +0100

    Add files via upload

---
+++ 16.12.2024/zadanie_6.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+//Zadanie 6 - Napisz funkcję, która wypisuje podany znak podaną liczbę razy. Parametry przekaż poprzez
+//wskaźniki.Na zakończenie funkcja powinna zmniejszać o 1, parametr odpowiadający za ilość
+//powtórzeń.
+
+void wypiszz(char* znak, int* iloscp)
+{
+	for (int i = 0; i < *iloscp; i++)
+	{
+		printf("%c", *znak);
+	}
+
+	(*iloscp)--;
+}
+
+int main()
+{
+	char znak;
+	int iloscp;
+
+	printf("Podaj znak do wypisania: \n");
+	znak = getchar(); //scanf_s do stringów jest do niczego zapamiętać!
+	printf("Podaj ilosc powtorzen z jakimi znak zostanie wypisany: \n");
+	scanf_s("%d", &iloscp);
+
+	wypiszz(&znak, &iloscp);
+
+	return 0;
+}
