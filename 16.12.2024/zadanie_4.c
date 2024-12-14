42f50f04abee5bc1b47686ccf23b383dc56abb82
Krystian <96619815+Avsh11@users.noreply.github.com>
2024-12-14 22:45:41 +0100

    Add files via upload

---
+++ 16.12.2024/zadanie_4.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+//Zadanie 4 - Napisz funkcję, która podnosi podaną liczbę do kwadratu. Parametr do funkcji przekaż poprzez
+//wskaźnik.
+
+void potega(float* n)
+{
+	*n = (*n) * (*n);
+}
+
+int main()
+{
+	float liczba;
+
+	printf("Podaj liczbe ktora zostanie spotegowana do kwadratu: \n");
+	scanf_s("%f", &liczba);
+
+	potega(&liczba);
+
+	printf("Liczba po spotegowaniu do kwadratu wynosi: %f \n", liczba);
+	
+	return 0;
+}
