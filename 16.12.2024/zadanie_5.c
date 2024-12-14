ec3300575a1a050f9bd6542ea0be4a9d15d241b7
Krystian <96619815+Avsh11@users.noreply.github.com>
2024-12-14 22:55:57 +0100

    Add files via upload

---
+++ 16.12.2024/zadanie_5.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+//Zadanie 5 - Napisz funkcję, która umożliwi policzenie podanej potęgi dla podanej liczby. Parametry
+// przekaż poprzez wskaźnik.Funkcja powinna zwrócić wskaźnik do wyniku.
+
+float* potega(float* podstawa, int* wykladnik)
+{
+	static float wynik;
+	wynik = 1.0;
+
+	for (int i = 0; i < *wykladnik; i++)
+	{
+		wynik *= *podstawa;
+	}
+
+	return &wynik;
+}
+
+int main()
+{
+	float liczba;
+	int wykladnik;
+
+	printf("Podaj liczbe do spotegowania: \n");
+	scanf_s("%f", &liczba);
+	printf("Podaj wykladnik potegi: \n");
+	scanf_s("%d", &wykladnik);
+
+	float* wynik = potega(&liczba, &wykladnik);
+
+	printf("Wynik potegowania to: %f \n", *wynik);
+
+	return 0;
+}
